e0c28387b8e9c85a29618698b2207873f22ea021
Nina Ong <azyvli@gmail.com>
2024-05-13 23:14:02 -0700

    updated google api stuff and added excel with results

--- google_api.py
+++ google_api.py
@@ -1,6 +1,7 @@
 import config
 import pathlib
 import textwrap
+import time
 
 import google.generativeai as genai
 
@@ -11,6 +12,16 @@ from IPython.display import Markdown
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+def safe_request(country, category, max_retries=5, delay=60):
+    for _ in range(max_retries):
+        try:
+            response = model.generate_content(f'Compose a 2-3 sentence explanation for the following: Status of #{category} in #{country}')
+            return response.text
+        except:
+            print("Retrying after 60 seconds.")
+            time.sleep(delay)  # Wait before retrying
+    return None
 
 # Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
 GOOGLE_API_KEY=config.GOOGLE_API_KEY
@@ -23,8 +34,7 @@ def interpret(countries, categories):
     for country in countries:
         results[country] = {}
         for category in categories:
-            response = model.generate_content(f'Compose a 2-3 sentence explanation for the following text: Status of #{category} in #{country}')
-            results[country][category] = response.text
+            results[country][category] = safe_request(country, category)
+            print(results[country][category])
     print('Results created')
     return results
-
